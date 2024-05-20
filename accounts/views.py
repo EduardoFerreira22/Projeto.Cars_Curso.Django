869591b2d014b2fb3e8c88642634b1f61f9e8146
EduardoFerreira22 <eduardoferreira_of@outlook.com>
2024-05-19 22:05:56 -0300

    Criação do app accounts

---
+++ accounts/views.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render
+
+# Create your views here.
+
+def register_view(request):
+    user_form = UserCreationForm()
+    return render(request, 'register.html', {'user_form': user_form})
